4114375d808cddad42d899dd43c5d1abdae6e7d3
MuardMukram <muardmukram@gmail.com>
2026-01-24 21:22:27 +0530

    login credintials

--- login.php
+++ login.php
@@ -10,7 +10,7 @@
 
     <h2>User Registration</h2>
 
-    <form method="POST" action="server.php">
+    <form method="POST" action="login.php">
         
 
 
@@ -20,6 +20,8 @@
         <label>Password:</label><br>
         <input type="password" name="password" required><br><br>
 
+                <input type="submit" value="Submit">
+
 
     </form>
 
@@ -28,6 +30,26 @@
 
 
 <?php
+
+include 'conn.php';
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+
+$username = $_POST['username'];
+$password = $_POST['password'];    
+
+echo "$username";
+echo "$password";
+
+$sql = "SELECT name FROM users WHERE username = '$username' AND password = '$password'";
+$result = $conn->query($sql);
+echo $result->num_rows;
+if ($result->num_rows > 0) {
+  // output data of each row
+    echo "Login successful. Welcome!";}
+} else {
+    echo "invalid credintials ";
+}
+$conn->close();
 
 ?>
 
