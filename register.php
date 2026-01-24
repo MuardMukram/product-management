8e90c2c10e913cd2721cf624ed31e74990369fdf
MuardMukram <muardmukram@gmail.com>
2026-01-24 20:27:56 +0530

    user registration

---
+++ register.php
@@ -0,0 +1,36 @@
+
+
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Simple Form</title>
+</head>
+<body>
+
+    <h2>User Registration</h2>
+
+    <form method="POST" action="server.php">
+        <label>Name:</label><br>
+        <input type="text" name="name" required><br><br>
+
+        <label>Username:</label><br>
+        <input type="text" name="username" required><br><br>
+
+        <label>Password:</label><br>
+        <input type="password" name="password" required><br><br>
+
+        <label>Email:</label><br>
+        <input type="email" name="email" required><br><br>
+
+        <input type="submit" value="Submit">
+    </form>
+
+</body>
+</html>
+
+
+
+
+
---
+++ server.php
@@ -0,0 +1,27 @@
+<?php
+
+$servername = "localhost";
+$db_username = "root";
+$db_password = "";
+$dbname = "practical1";
+
+// Create connection
+$conn = new mysqli($servername, $db_username, $db_password, $dbname);
+
+// Check connection
+if ($conn->connect_error) {
+  die("Connection failed: " . $conn->connect_error);
+}
+echo "Connected successfully";
+
+$name = $_POST['name'];
+$username = $_POST['username'];
+$password = $_POST['password'];    
+$email = $_POST['email'];
+
+echo $name; 
+echo $username;
+echo $password; 
+
+$sql = "INSERT INTO users (name, username, password, email) values('name', 'sername', 'assword', 'mail')";
+?>
